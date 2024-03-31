a5c570473c6b6ca7bc7238c2d2aa7953e50c2b8b
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-31 22:17:26 +0100

    parser added to __main__.py

--- src/phase_rotate_ms/__main__.py
+++ src/phase_rotate_ms/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import time
 
@@ -5,6 +6,38 @@ import time
 def main() -> None:
     """
     """
+
+def parse_args() -> argparse.Namespace:
+    """
+    """
+    parser = argparse.ArgumentParser(
+        description="MeasurementSet reader for radio astronomy",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "ms_dir",
+        type=str,
+        help="input MeasurementSet directory."
+    )
+    parser.add_argument(
+        "new_phase_centre",
+        type=float,
+        nargs=2,
+        help="new phase centre in RA DEC."
+    )
+    parser.add_argument(
+        "--name",
+        type=str,
+        default="output",
+        help="name for output dir (default=output)."
+    )
+    parser.add_argument(
+        "--rm",
+        type=bool,
+        action="store_true",
+        help="overwrite existing file with same name"
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
