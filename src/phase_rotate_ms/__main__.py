10b118c022608e6a531677561357c50e49a6abde
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-31 22:10:41 +0100

    __main__.py added

---
+++ src/phase_rotate_ms/__main__.py
@@ -0,0 +1,13 @@
+import logging
+import time
+
+
+def main() -> None:
+    """
+    """
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    logging.info(f"Full time = {time.time() - start_time} s")
