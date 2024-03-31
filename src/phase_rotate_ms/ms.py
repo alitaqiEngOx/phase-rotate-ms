98057198d762e5f85c908a9be2397917fe1eed86
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-31 18:13:25 +0100

    phase centre reader method added

--- src/phase_rotate_ms/ms.py
+++ src/phase_rotate_ms/ms.py
@@ -1,6 +1,10 @@
+import os
 from dataclasses import dataclass
-
 from pathlib import Path
+
+import numpy as np
+from astropy.coordinates import SkyCoord
+from casacore.tables import table
 
 
 @dataclass
@@ -11,4 +15,22 @@ class MS:
     dir: Path
     """"""
 
-    
+    @property
+    def phase_centre(self) -> SkyCoord:
+        """
+        MeasurementSet phase centre.
+        """
+        try:
+            with tools.block_logging():
+                phase_centre = table(
+                    str(self.dir.joinpath("FIELD"))
+                ).getcol("PHASE_DIR")
+        except:
+            raise FileNotFoundError(
+                "expected a 'FIELD' table with a 'PHASE_DIR' column"
+            )
+        if np.array(phase_centre).shape != (1, 1, 2):
+            raise ValueError("unsupported phase centre definition")
+        return SkyCoord(
+            phase_centre[0][0][0], phase_centre[0][0][1], unit="rad"
+        )
